aaeff8f8c7023a17ea119783c7742b4a5d419fe9
Rufat-J <rj.7@live.se>
2023-12-05 14:29:21 +0100

    updated user controller

--- src/main/java/code/me/springapi/controller/UserController.java
+++ src/main/java/code/me/springapi/controller/UserController.java
@@ -1,39 +1,35 @@
 package code.me.springapi.controller;
 
-
-import code.me.springapi.model.User;
-import code.me.springapi.security.JwtTokenHandler;
+import code.me.springapi.dto.LoginSuccess;
+import code.me.springapi.dto.RegisterSuccess;
 import code.me.springapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
-
-    private record UserDTO(String username, String password) {};
-
-    private JwtTokenHandler jwtTokenHandler;
+    private record UserDTO(String username, String password){};
 
     @Autowired
-    public UserController(UserService userService, JwtTokenHandler jwtTokenHandler) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.jwtTokenHandler = jwtTokenHandler;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<RegisterSuccess> registerUser(@RequestBody UserDTO userDTO){
         var result = userService.register(userDTO.username(), userDTO.password());
         return ResponseEntity.ok().body(result);
     }
 
-    @PostMapping("login")
-    public String login(@RequestBody UserDTO user) {
+    @PostMapping("/login")
+    public ResponseEntity<LoginSuccess> login(@RequestBody UserDTO user) {
         var result = userService.login(user.username(), user.password());
-        return result;
+        return ResponseEntity.ok().body(result);
     }
+
 }
