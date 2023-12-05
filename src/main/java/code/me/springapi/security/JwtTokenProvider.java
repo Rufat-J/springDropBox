a58d291e931745a1605367edaa73e0fbe147f0b8
Rufat-J <rj.7@live.se>
2023-12-05 14:27:06 +0100

    Jwt provider

---
+++ src/main/java/code/me/springapi/security/JwtTokenProvider.java
@@ -0,0 +1,55 @@
+package code.me.springapi.security;
+
+import code.me.springapi.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+@NoArgsConstructor
+public class JwtTokenProvider {
+
+    private final String secret = "keyboardcat-testwerwerwerwerwererwwerwerwerwetgwegwegwegwerwerwerwerwerwerwerwerw234234234wefr2342f234";
+    private final Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+    public String generateToken(User user) {
+        long id = user.getId();
+        String username = user.getUsername();
+
+        return Jwts.builder()
+                .setSubject(Long.toString(id))
+                .claim("id", id)
+                .claim("username", username)
+                .signWith(key)
+                .compact();
+    }
+
+    public int getTokenId(String token) {
+        return getTokenClaim(token, "id", Integer.class);
+    }
+
+    public <T> T getTokenClaim(String token, String type, Class<T> returnType) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.get(type, returnType);
+    }
+
+    public boolean validate(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
