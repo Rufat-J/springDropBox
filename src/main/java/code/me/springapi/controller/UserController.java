ff872e3fcb6c2438157ed4cf3ad2e5bb97c0fb55
Rufat-J <rj.7@live.se>
2023-12-07 13:16:01 +0100

    created folderController with 2 endpoints

--- src/main/java/code/me/springapi/controller/UserController.java
+++ src/main/java/code/me/springapi/controller/UserController.java
@@ -2,14 +2,19 @@ package code.me.springapi.controller;
 
 import code.me.springapi.dto.LoginSuccess;
 import code.me.springapi.dto.RegisterSuccess;
+import code.me.springapi.model.Folder;
+import code.me.springapi.model.User;
 import code.me.springapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
